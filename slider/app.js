96b2532beb957f0be1f675a7abf3633866413134
Zuzia <92038650+xHarleen@users.noreply.github.com>
2023-03-11 18:36:41 +0100

    slider

---
+++ slider/app.js
@@ -0,0 +1,46 @@
+let activeSlideNumber = 1;
+let arrowLeft = document.querySelector('.arrow-left');
+let arrowRight = document.querySelector('.arrow-right');
+
+let hideActiveSlide = () => {
+    let activeElement = document.querySelector('.active');
+    activeElement.classList.remove('active');
+};
+
+let showSlide = (slideNumber) => {
+    hideActiveSlide();
+    document.querySelector('#slide'+slideNumber).classList.add('active');
+};
+
+let showNextSlide = () => {
+    if(activeSlideNumber === 3 ){
+        activeSlideNumber = 1;
+    }
+    else {
+        activeSlideNumber = activeSlideNumber + 1;
+    }
+    
+    showSlide(activeSlideNumber);
+}
+
+let showPreviousSlide = () => {
+    if(activeSlideNumber === 1 ){
+        activeSlideNumber = 3;
+    }
+    else {
+        activeSlideNumber = activeSlideNumber - 1;
+    }
+    
+    showSlide(activeSlideNumber);
+}
+
+for (let i = 1; i <= 3; i++) { 
+    let showSlideI = () => {
+        activeSlideNumber = i;
+        showSlide(i);
+    };
+    document.querySelector('#dot'+i).addEventListener('click',showSlideI);
+}
+
+arrowRight.addEventListener('click', showNextSlide);
+arrowLeft.addEventListener('click', showPreviousSlide);
